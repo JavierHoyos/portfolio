b91d6090b515c97e7b9d130195dcd6c623dc7cb0
Javier Hoyos <hoyosjavier56@gmail.com>
2022-07-23 16:13:50 -0500

    se agrega el proyecto

---
+++ light-blog3.js
@@ -0,0 +1,7 @@
+
+    // Toggle theme
+    const  themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () =>{
+        let element = document.body;
+        element.classList.toggle('light-mode3')
+    })
